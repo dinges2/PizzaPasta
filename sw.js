fffba098589733de346f9b0f7e534145789da4e1
dinges2 <0993362@hr.nl>
2022-11-02 15:00:40 +0100

    added sw

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
 self.addEventListener("install", e => {
-    console.log("Installing sw");
+    console.log("Caching resources..");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "./src/manifestus.json",
+                "./src/images/GET.png"
+            ]);
+        })
+    );
+    console.log("Resources in cache. Done");
 });
